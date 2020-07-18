a6c279fc75634b8473b65c5ad7296bdc9d3759ce
hayashidamoka <sudekamo1827@gmail.com>
2020-07-18 23:15:22 +0900

    エラー直し中

--- app/src/main/Kotlin/jp/co/pannacotta/lunch_app/model/Gourmet.kt
+++
@@ -1,9 +0,0 @@
-package jp.co.pannacotta.lunch_app.model
-
-import org.parceler.Parcel
-
-@Parcel
-class Gourmet {
-    @JvmField
-    var results: Results? = null
-}
--- app/src/main/Kotlin/jp/co/pannacotta/lunch_app/model/Results.kt
+++
@@ -1,9 +0,0 @@
-package jp.co.pannacotta.lunch_app.model
-
-import org.parceler.Parcel
-
-@Parcel
-class Results {
-    @JvmField
-    var shop: List<Shop>? = null
-}
