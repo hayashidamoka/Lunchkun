6cfefb4a03e22c2bf9e3adfb2fd7bad281e5af7a
hayashidamoka <sudekamo1827@gmail.com>
2020-07-18 23:19:29 +0900

    エラー直し中

---
+++ app/src/main/java/jp/co/pannacotta/lunch_app/model/Photo.kt
@@ -0,0 +1,9 @@
+package jp.co.pannacotta.lunch_app.model
+
+import org.parceler.Parcel
+
+@Parcel
+class Photo {
+    @JvmField
+    var pc: Pc? = null
+}
---
+++ app/src/main/java/jp/co/pannacotta/lunch_app/model/Urls.kt
@@ -0,0 +1,9 @@
+package jp.co.pannacotta.lunch_app.model
+
+import org.parceler.Parcel
+
+@Parcel
+class Urls {
+    @JvmField
+    var pc: String? = null
+}
